cbad9a106b32c46fb20d62380d25677e4bf88642
Weiyi Kong <kongwy@users.noreply.github.com>
2020-09-05 18:49:58 +0800

    Initial Commit

---
+++ RBG Explorer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RBG Explorer
+//
+//  Created by Weiyi Kong on 5/9/20.
+//  Copyright © 2020 Weiyi Kong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
